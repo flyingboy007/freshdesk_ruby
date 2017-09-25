6ee21cf319de81bb8c90b935169030c5eb9a156b
flyingboy007 <abhilashvr007@gmail.com>
2017-09-26 00:36:18 +0530

    added ability to view all articles under a folder

--- lib/freshdesk/solution.rb
+++ lib/freshdesk/solution.rb
@@ -19,6 +19,26 @@ module Freshdesk
         response['status']
       end
     end
+
+    def self.list_all_articles(query)
+      uri = URI.parse(Freshdesk.configuration.url+"/api/v2/solutions/folders/#{query}/articles")
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = true
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+      request = Net::HTTP::Get.new(uri)
+      request.basic_auth(Freshdesk.configuration.api_key, "X")
+      #request["content-type"] = 'application/json'
+      response = http.request(request)
+
+
+      if Freshdesk.valid_json?(response.body)
+        JSON.parse(response.body, :symbolize_names => true)
+      else
+        #this is string response during error cases
+        response['status']
+      end
+    end
   end
 end
 
--- spec/freshdesk/solution_spec.rb
+++ spec/freshdesk/solution_spec.rb
@@ -27,6 +27,31 @@ describe Freshdesk::Solution do
     end
 
   end
+ context 'view all articles under a folder' do
+    it 'is success' do
+      VCR.use_cassette('freshdesk/solutions/view_all_articles/success') do
+        solutions_id=52525252
+        response=Freshdesk::Solution.list_all_articles(solutions_id)
+        expect(response).to be_an_instance_of(Array)
+      end
+    end
+
+    it 'with no tickets email' do
+      no_existing_query=1
+      VCR.use_cassette('freshdesk/solutions/view_all_articles/with_non_existing_id') do
+        response=Freshdesk::Solution.list_all_articles(no_existing_query)
+        expect(response).to eq('404 Not Found')
+      end
+    end
+
+    it 'with invalid query parameter' do
+      VCR.use_cassette('freshdesk/solutions/view_all_articles/invalid_query_param') do
+        response=Freshdesk::Solution.list_all_articles("invalid=something")
+        expect(response).to eq('404 Not Found')
+      end
+    end
+
+  end
 
 
 

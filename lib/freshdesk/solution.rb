f012f68820d5f909c56bb2108deaa910a838ce94
flyingboy007 <abhilashvr007@gmail.com>
2017-09-26 10:31:16 +0530

    added ability to view get article

--- lib/freshdesk/solution.rb
+++ lib/freshdesk/solution.rb
@@ -39,6 +39,26 @@ module Freshdesk
         response['status']
       end
     end
+
+    def self.get_article(query)
+      uri = URI.parse(Freshdesk.configuration.url+"/api/v2/solutions/articles/#{query}")
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = true
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+      request = Net::HTTP::Get.new(uri)
+      request.basic_auth(Freshdesk.configuration.api_key, "X")
+      #request["content-type"] = 'application/json'
+      response = http.request(request)
+
+
+      if Freshdesk.valid_json?(response.body)
+        JSON.parse(response.body, :symbolize_names => true)
+      else
+        #this is string response during error cases
+        response['status']
+      end
+    end
   end
 end
 
--- spec/freshdesk/solution_spec.rb
+++ spec/freshdesk/solution_spec.rb
@@ -2,10 +2,10 @@ require_relative '../spec_helper'
 
 describe Freshdesk::Solution do
 
- context 'view all folders under a category' do
+  context 'view all folders under a category' do
     it 'is success' do
       VCR.use_cassette('freshdesk/solutions/view_all_solutions/success') do
-        category_id=76767676
+        category_id=35000018943
         response=Freshdesk::Solution.list_all_folders(category_id)
         expect(response).to be_an_instance_of(Array)
       end
@@ -27,10 +27,10 @@ describe Freshdesk::Solution do
     end
 
   end
- context 'view all articles under a folder' do
+  context 'view all articles under a folder' do
     it 'is success' do
       VCR.use_cassette('freshdesk/solutions/view_all_articles/success') do
-        solutions_id=52525252
+        solutions_id=35000028485
         response=Freshdesk::Solution.list_all_articles(solutions_id)
         expect(response).to be_an_instance_of(Array)
       end
@@ -53,6 +53,31 @@ describe Freshdesk::Solution do
 
   end
 
+  context 'Getting an Article' do
+    it 'is success' do
+      VCR.use_cassette('freshdesk/solutions/get_one_article/success') do
+        article_id=35000002296
+        response=Freshdesk::Solution.get_article(article_id)
+        expect(response[:id]).to eq(article_id)
+      end
+    end
+
+    it 'with no tickets email' do
+      no_existing_query=1
+      VCR.use_cassette('freshdesk/solutions/get_one_article/with_non_existing_id') do
+        response=Freshdesk::Solution.get_article(no_existing_query)
+        expect(response).to eq('404 Not Found')
+      end
+    end
+
+    it 'with invalid query parameter' do
+      VCR.use_cassette('freshdesk/solutions/get_one_article/invalid_query_param') do
+        response=Freshdesk::Solution.get_article("invalid=something")
+        expect(response).to eq('404 Not Found')
+      end
+    end
+
+  end
 
 
 end
